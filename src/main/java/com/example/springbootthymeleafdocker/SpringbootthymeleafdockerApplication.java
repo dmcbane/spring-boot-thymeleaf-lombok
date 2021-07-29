5848d62bd4b237918d817254db32d2ddd5ca24e1
H. Dale McBane <h.dale.mcbane@gmail.com>
2021-07-29 17:02:33 -0400

    Initial Commit – from Spring Initializr

---
+++ src/main/java/com/example/springbootthymeleafdocker/SpringbootthymeleafdockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootthymeleafdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootthymeleafdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootthymeleafdockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootthymeleafdocker/SpringbootthymeleafdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootthymeleafdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootthymeleafdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
